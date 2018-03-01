9fa05f3e85081889b0d54e90d0a50b8072f114a2
Barry Russell <blrussell09@gmail.com>
2018-03-01 17:14:00 -0600

    Added challenge 3 and 4

--- main.js
+++ main.js
@@ -27,3 +27,28 @@ function wordScore(string) {
 
  var divElement = document.getElementById('challenge-1')
  divElement.innerHTML = "<h4>" + highScore + "</h4>";
+
+
+//  Challenge 2
+var threeInputA = [1,2,3,4,5,6,7,8,9];
+var inputAjoin = threeInputA.join('');
+
+var blalala = function padawan(switcher) {
+  return switcher.map(x => parseInt(x.toString().replace(/3/g, '~').replace(/7/g, '3').replace(/~/g, '7')));
+  
+};
+
+var notherDivElement = document.getElementById('challenge-2');
+notherDivElement.innerHTML = "<h4>" + blalala(threeInputA) + "</h4>"
+
+// Challenge 3 
+var challengeThreeInputA = [ 1, 5, 7, 2, 5, 1 ]
+
+var unique = function uniq(arr1) {
+  return arr1.filter(function(elem, index, self) {
+    return self.indexOf(elem)==self.lastIndexOf(elem);
+  });
+}
+
+var aNotherDivElement = document.getElementById('challenge-3');
+aNotherDivElement.innerHTML = "<h4>" + unique(challengeThreeInputA) + "</h4>"
