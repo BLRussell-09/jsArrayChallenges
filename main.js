a446827a04db32a355f77da75408420ed1fb49de
Barry Russell <blrussell09@gmail.com>
2018-03-02 12:32:46 -0600

    Added challenges 5 - 8

--- main.js
+++ main.js
@@ -52,3 +52,76 @@ var unique = function uniq(arr1) {
 
 var aNotherDivElement = document.getElementById('challenge-3');
 aNotherDivElement.innerHTML = "<h4>" + unique(challengeThreeInputA) + "</h4>"
+
+// Challenge 4
+
+var array1 = [1,2,3,4,5,6,7,8,9,10]
+var dubz = array1.map(x => x * 2);
+
+var divSix = document.getElementById('challenge-4');
+divSix.innerHTML = "<h4>" + dubz + "</h4>"
+
+// Challenge 5
+
+var arr1 = [1,2];
+var arr2 = [1];
+
+Array.prototype.diff = function (a) {
+  return this.filter(function (i) {
+      return a.indexOf(i) === -1;
+  });
+};
+
+var divFive = document.getElementById('challenge-5');
+divFive.innerHTML = "<h4>" + arr1.diff(arr2) + "</h4>"
+
+// Challenge 6
+
+var arr = [1,2,3,4,6,7,8,10];
+var lowEnd = Math.min.apply(null, arr);
+var highEnd = Math.max.apply(null, arr);
+var list = [];
+
+for (var i = lowEnd; i <= highEnd; i++) {
+  list.push(i);
+}
+
+
+
+var divSix = document.getElementById('challenge-6');
+divSix.innerHTML = "<h4>" + list.diff(arr) + "</h4>"
+
+// Challenge 7
+var begArr = [1,2,3,4,5]
+var beg1 = []
+var beg2 = []
+for (var idx = 0; idx < begArr.length; idx++)
+{
+  if (begArr[idx] % 2 === 0)
+  {
+    beg1.push(begArr[idx]);
+  } 
+  else
+  {
+    beg2.push(begArr[idx]);
+  }
+}
+
+// Challenge 8
+
+var test = 23434567; 
+var testSplat = test.toString();
+var output = '';
+for (var idx = 0; idx < testSplat.length; idx++) {
+  if (idx === 0 )
+  {
+    output = testSplat[idx]
+  }
+  else
+  {
+    output += "-" + testSplat[idx];
+  }
+}
+
+var divOcho = document.getElementById('challenge-8');
+divSix.innerHTML = "<h4>" + output + "</h4>"
